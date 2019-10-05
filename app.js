fd29d7290721d79592c6f980722cdb9095aa31f8
renjitasony <renjitasony@gmail.com>
2019-10-05 16:25:29 +0530

    express demo apply styles

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express=require("express");
 const chalk=require('chalk');
+const path=require('path');
 var app=new express();
+app.use(express.static(path.join(__dirname,"/public")));
 app.all('/',function(req,res){
-    res.send("Hello world prince");
+   res.sendFile(__dirname+"/index.html");
 });
 app.listen(3000,()=>{
     console.log("listening to port"+chalk.green('3000'));
